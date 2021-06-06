ef3589d462d46644e55750fe585896405a30c801
lucifer20042004 <85445084+lucifer20042004@users.noreply.github.com>
2021-06-06 16:03:23 +0200

    Add files via upload

---
+++ File_Bot/Reply.lua
@@ -0,0 +1,225 @@
+local function Reply(msg)
+local text = msg.content_.text_
+if not database:get(bot_id..'Reply:Status'..msg.chat_id_) then
+
+
+if text then 
+list = {'صباح الخير'}
+for k,v in pairs(list) do
+if string.find(text,v) ~= nil then
+send(msg.chat_id_,msg.id_, '[انت الخير ياعمري..❤️](t.me/T_w_i_n_s_1_1)') 
+return false
+end
+end
+end
+
+if text then 
+list = {'جيت'}
+for k,v in pairs(list) do
+if string.find(text,v) ~= nil then
+send(msg.chat_id_,msg.id_, '[لف وارجع تانيي مش حوارر😂💃](t.me/T_w_i_n_s_1_1)') 
+return false
+end
+end
+end
+
+
+
+if text then 
+list = {'🌚'}
+for k,v in pairs(list) do
+if string.find(text,v) ~= nil then
+send(msg.chat_id_,msg.id_, '[قمر اسود🌚🖤](t.me/T_w_i_n_s_1_1)') 
+return false
+end
+end
+end
+
+if text then 
+list = {'هموت'}
+for k,v in pairs(list) do
+if string.find(text,v) ~= nil then
+send(msg.chat_id_,msg.id_, '[مع الف سلامه😁🔥](tme/T_w_i_n_s_1_1)') 
+return false
+end
+end
+end
+
+
+if text then 
+list = {'زعلان'}
+for k,v in pairs(list) do
+if string.find(text,v) ~= nil then
+send(msg.chat_id_,msg.id_, '[عادي يتفلق هنعملو اي..😒💔](t.me/T_w_i_n_s_1_1)') 
+return false
+end
+end
+end
+
+
+if text then 
+list = {'حصل'}
+for k,v in pairs(list) do
+if string.find(text,v) ~= nil then
+send(msg.chat_id_,msg.id_, '[حصل حصوله..😹💜](t.me/T_w_i_n_s_1_1)') 
+return false
+end
+end
+end
+
+
+if text == 'فرعون'or text == 'سلفستر' then 
+send(msg.chat_id_,msg.id_, '[☆DEV SELVESTR](t.me/F_R_3_O_N)')
+return false
+end
+
+if text == 'المبرمج' or text == 'مبرمج السورس' or text == 'فامبير' or text == 'فمبير' then
+send(msg.chat_id_,msg.id_, '[✪DEV VAMBAIR](t.me/V_a_a_m_b_a_I_r)')
+return false
+end
+
+if text == 'انا مين' then 
+send(msg.chat_id_,msg.id_, '[نت احلى حاجه فحياتي ❤️🍃](t.me/T_w_i_n_s_1_1)') 
+return false
+end
+
+if text == 'لوسيفر' or text == 'يا لوسيفر' then 
+send(msg.chat_id_,msg.id_, '[★DEV LUSEFAR](t.me/LLucifeerr)
+return false
+end
+
+if text == 'تيست' then 
+send(msg.chat_id_,msg.id_, ' البوت شغال✵ ') 
+return false
+end
+
+
+if text == 'سلام' then 
+send(msg.chat_id_,msg.id_, '[ابق تعاله كليوم..😹💔🎶](t.me/T_w_i_n_s_1_1)') 
+return false
+end
+
+if text == 'هاي' or text == 'هيي' then
+send(msg.chat_id_,msg.id_, '[علي الواي فاي..😺💜](t.me/T_w_i_n_s_1_1)') 
+return false
+end
+
+if text then 
+list = {'برايفت'}
+for k,v in pairs(list) do
+if string.find(text,v) ~= nil then
+send(msg.chat_id_,msg.id_, '[خدوني معاكم برايفت والنبي..🥺💜](t.me/T_w_i_n_s_1_1)') 
+return false
+end
+end
+end
+
+if text then 
+list = {'علي النبي'}
+for k,v in pairs(list) do
+if string.find(text,v) ~= nil then
+send(msg.chat_id_,msg.id_, '[عليه الصلاه والسلام..💛🙂](t.me/T_w_i_n_s_1_1)') 
+return false
+end
+end
+end
+
+if text then 
+list = {'قفل المحن'}
+for k,v in pairs(list) do
+if string.find(text,v) ~= nil then
+send(msg.chat_id_,msg.id_, 'اهلا عزيزي تم قفل المحن بنجاح اتمحونوا بف عشان المراره 😹🌚') 
+return false
+end
+end
+end
+
+if text then 
+list = {'🙄'}
+for k,v in pairs(list) do
+if string.find(text,v) ~= nil then
+send(msg.chat_id_,msg.id_, '[نزل عينك عيب كده..🌚♥️](t.me/T_w_i_n_s_1_1)') 
+return false
+end
+end
+end
+
+if text then 
+list = {'فتح المحن'}
+for k,v in pairs(list) do
+if string.find(text,v) ~= nil then
+send(msg.chat_id_,msg.id_, 'اهلا عزيزي تم فتح المحن بنجاح') 
+return false
+end
+end
+end
+
+--fgf
+if text == "فامبير" or text == "فمبير" or text == "المبرمج" then
+
+send(msg.chat_id_,msg.id_, '[DEV VAMBAIR](t.me/V_a_a_m_b_a_I_r)')
+return false
+end
+
+if text then 
+list = {'💋'}
+for k,v in pairs(list) do
+if string.find(text,v) ~= nil then
+send(msg.chat_id_,msg.id_, '[عايز من ده..💋🥀](t.me/T_w_i_n_s_1_1)') 
+return false
+end
+end
+end
+
+if text then 
+list = {'ب ف'}
+for k,v in pairs(list) do
+if string.find(text,v) ~= nil then
+send(msg.chat_id_,msg.id_, '[خدوني معاكم بف..🙄💔](t.me/T_w_i_n_s_1_1)') 
+return false
+end
+end
+end
+
+if text then 
+list = {'😔'}
+for k,v in pairs(list) do
+if string.find(text,v) ~= nil then
+send(msg.chat_id_,msg.id_, '[متزعلش بحبك..😥♥️](t.me/T_w_i_n_s_1_1)') 
+return false
+end
+end
+end
+
+if text then 
+list = {'سلام عليكم'}
+for k,v in pairs(list) do
+if string.find(text,v) ~= nil then
+send(msg.chat_id_,msg.id_, '[وعليكم السلام ..🖤🌚](t.me/T_w_i_n_s_1_1)') 
+return false
+end
+end
+end
+
+--tt
+if text == "بحبك"  or text == "بتحبني" then
+
+send(msg.chat_id_,msg.id_, '[بعشقك ياروحي..🤗💜](t.me/T_w_i_n_s_1_1)')
+return false
+end
+
+if text == 'تفعيل ردود البوت' and Manager(msg) then
+database:del(bot_id..'repdark:Status'..msg.chat_id_)
+send(msg.chat_id_, msg.id_,'◍ تم تفعيل الردود بنجاح')
+return false
+end
+
+if text == 'تعطيل ردود البوت' and Manager(msg) then
+database:set(bot_id..'repdark:Status'..msg.chat_id_,true)
+send(msg.chat_id_, msg.id_,'◍ تم تعطيل الردود بنجاح')
+return false
+end
+end
+return {
+Poyka = Reply
+}
